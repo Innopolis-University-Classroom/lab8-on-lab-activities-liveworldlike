7e15157e74cf4e5bc1f4dac723dac3d212fc95a2
liveworldlike <liveworldlike@gmail.com>
2022-03-21 15:31:18 +0300

    Moved files from "wrong" folders to "correct" folders

---
+++ src/com/muwaffaq/innopolis/solid/LSP_IS/correct/Bird.java
@@ -0,0 +1,18 @@
+package com.muwaffaq.innopolis.solid.LSP_IS.wrong;
+
+
+
+interface Waterfown {
+    void swim();
+}
+
+interface Flying{
+    void fly();
+
+}
+
+interface Bird   {
+    void hatch();
+
+    void eat();
+}
